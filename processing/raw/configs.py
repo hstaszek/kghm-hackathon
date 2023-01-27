3ef962d9539675a92a37d87f818bf3ab17f48981
Dominik Kaczmarek <dominik.kaczmarek@britenet.com.pl>
2023-01-27 20:47:55 +0100

    update extract_fn

---
+++ processing/raw/configs.py
@@ -0,0 +1,14 @@
+csv_in_parameters = {
+    "sep": ",",
+    "header": 0,
+    "skip_blank_lines": True,
+    "encoding": "utf-8"
+}
+
+csv_out_parameters = {
+    "mode": "w",
+    "index": True,
+    "index_label": "idx",
+    "sep": ",",
+    "encoding": "utf-8",
+}
--- processing/raw/extract.py
+++ processing/raw/extract.py
@@ -1,33 +1,31 @@
 import logging
 import os
+import json
 
 import pandas as pd
 
+from processing.raw.configs import csv_in_parameters, csv_out_parameters
 from processing.raw.schema import RAW_SCHEMA
 
 log = logging.getLogger('base')
 
-csv_in_parameters = {
-    "sep": ",",
-    "header": 0,
-    "skip_blank_lines": True,
-    "encoding": "utf-8"
-}
-csv_out_parameters = {
-    "mode": "w",
-    "index": True,
-    "index_label": "idx",
-    "sep": ",",
-    "encoding": "utf-8",
-}
 
-
-def extract_fn(src_dir: str, target_schema_path: str, target_path: str):
-    target_schema = pd.read_excel(target_schema_path, dtype={"tagname": "object"}, usecols=[0])
+def parse_schema(schema_path: str):
+    target_schema = pd.read_excel(schema_path, dtype={"tagname": "object"}, usecols=[0])
     target_schema = target_schema["tagname"].tolist()
     target_schema = {"Czas": "object", **{col: "float64" for col in target_schema}}
-    log.info(f"target schema: {target_schema}")
 
+    target_schema_path = os.path.join(
+        os.path.dirname(schema_path),
+        os.path.basename(schema_path).replace("xlsx", "json")
+    )
+    with open(target_schema_path, "w") as f:
+        json.dump(target_schema, f)
+    log.info(f"target schema: {target_schema_path}")
+    return target_schema_path
+
+
+def extract_fn(src_dir: str, target_path: str):
     loading_paths = []
     for path in os.listdir(src_dir):
         if path.endswith(".csv"):
@@ -41,12 +39,12 @@ def extract_fn(src_dir: str, target_schema_path: str, target_path: str):
     target_df = None
     is_initial = True
     for i, path in enumerate(loading_paths):
-        log.info(f"{i+1}/{loading_paths_number} processing: {path}")
+        log.info(f"{i + 1}/{loading_paths_number} processing: {path}")
         var_df = pd.read_csv(path, dtype=RAW_SCHEMA, usecols=["Zmienna", "Wartosc", "Czas"], **csv_in_parameters)
 
         unique_vars = var_df["Zmienna"].unique()
         if len(unique_vars) > 1:
-            log.info(f"{i+1}/{loading_paths_number} ! more than 2 vars {unique_vars}")
+            log.info(f"{i + 1}/{loading_paths_number} ! more than 2 vars {unique_vars}")
 
         for var in unique_vars:
             side_var_df = var_df[var_df["Zmienna"] == var]
@@ -58,8 +56,5 @@ def extract_fn(src_dir: str, target_schema_path: str, target_path: str):
             target_df = target_df.merge(side_var_df, how="outer", on="Czas")
 
     log.info(f"saving target file: {target_path}")
-    target_df.to_csv(
-        target_path,
-        **csv_out_parameters
-    )
+    target_df.to_csv(target_path, **csv_out_parameters)
     log.info("extract_fn ends")
