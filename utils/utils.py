28f130f1497f13fa1082a8a6d0852caca71f0b8c
Ignacy Stepka <ignacys@man.poznan.pl>
2023-01-27 23:50:35 +0100

    Add utils functions to get consistent process of removing irrelevant blacklisted features

---
+++ utils/utils.py
@@ -0,0 +1,40 @@
+import pandas as pd
+import numpy as np
+
+def remove_blacklisted(df: pd.DataFrame, file: str = 'blacklist15.txt') -> pd.DataFrame:
+    '''
+    Remove dataframe columns based on names in blacklist text file.
+    '''
+    blacklist = list()
+
+    # Drop blacklisted (correlated) columns
+    with open(file, 'r') as f:
+        lines = f.readlines()
+        for line in lines:
+            blacklist.append(line[:-1])
+
+    blacklist = np.array(blacklist, dtype='str')
+
+    to_drop = list()
+    for col in df.columns.to_list():
+        if col in blacklist:
+            to_drop.append(col)    
+
+    _df = df.drop(columns=to_drop)
+    return _df
+
+def remove_redundant_columns(df: pd.DataFrame) -> pd.DataFrame:
+    '''
+    Remove columns like 'Czas' and 'idx' if they appear in DF and remove zero-std features.
+    '''
+    if 'Czas' in df.columns:
+        df = df.drop(columns='Czas')
+
+    if 'idx' in df.columns:
+        df = df.drop(columns='idx')
+
+    
+    columns_without_zeros = df.columns.to_numpy()[~np.isclose(df.std(), 0.0, rtol=1e-4)]
+
+
+    return df[columns_without_zeros]
