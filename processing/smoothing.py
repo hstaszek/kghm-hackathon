b8b99b05041343abe029f7e4b790b519386def1b
Ignacy Stepka <ignacy.stepka@gmail.com>
2023-01-29 02:39:31 +0100

    Add smoothing utility functions

--- processing/smoothing.py
+++ processing/smoothing.py
@@ -41,6 +41,44 @@ def wma_smoothing(df: pd.DataFrame, window_size: int = 5, outlier_n_stdev: int =
 
     return smoothed_df
 
+def expo_smoothing(df: pd.DataFrame, window_size: int = 5, outlier_n_stdev: int = 2, ignore_columns: List[str] = []) -> pd.DataFrame:
+    '''
+    Apply expo smoothing.  
+    '''
+
+    raw_df: pd.DataFrame = df.copy().drop(columns=ignore_columns)
+
+    #smoothed = list()
+    smoothed = np.zeros(shape=raw_df.shape)
+
+    stdevs = raw_df.std(axis=0)
+    means = raw_df.mean(axis=0)
+
+    print(stdevs)
+    print(means)
+
+    wmas = np.ones(shape=(window_size, raw_df.shape[1]))  * np.array([means for i in range(window_size)])
+
+
+    for i, row in enumerate(df.values):
+        wmas[range(window_size-1)] = wmas[range(1, window_size)] 
+        wmas[window_size-1] = row
+
+        outlier_mask = np.abs((row - means) / stdevs) > outlier_n_stdev
+
+        wmas[window_size-1] = row
+        wmas[window_size-1][outlier_mask] = np.mean(wmas[0:window_size-1, :], axis=0)[outlier_mask]
+
+        smoothed[i] = wmas[window_size-1]
+
+
+    smoothed_df = pd.DataFrame(data=smoothed, columns=raw_df.columns)
+
+    if len(ignore_columns) > 0:
+        pd.concat([smoothed_df, df[ignore_columns]], ignore_index=True)
+
+    return smoothed_df
+
 
 r = pd.read_csv('tmp.csv')
 wma_smoothing(r)
--- utils/utils.py
+++ utils/utils.py
@@ -4,6 +4,8 @@ from typing import List
 import pandas as pd
 import numpy as np
 
+from statsmodels.tsa.api import ExponentialSmoothing, SimpleExpSmoothing, ARIMA
+
 #from grupy import grupy_sekcji_1
 
 
@@ -59,11 +61,23 @@ def remove_redundant_columns(df: pd.DataFrame) -> pd.DataFrame:
     return df[columns_without_zeros]
 
 
-def filter_device_groups(df: pd.DataFrame, file: str, groups: List[str], y_name: str):
+def filter_device_groups(df: pd.DataFrame, file: str, groups: List[str], y_name: str,  anti_groups: List[str] = []):
     with open(file, 'r') as f:
         json_load = json.load(f)
     unloaded_lists = [col for group in groups for col in json_load.get(group)]
-    selected_list = [col for device in unloaded_lists for col in df.columns if device in col and device not in y_name] + [y_name]
+    unloaded_anty_lists = [col for group in anti_groups for col in json_load.get(group)]
+    
+    selected_list = list()
+    for device in unloaded_lists:
+        for col in df.columns:
+            if device in col:
+                allow = True
+                for anty_device in unloaded_anty_lists:
+                    if anty_device in col:
+                        allow = False
+                if allow:
+                    selected_list.append(col)
+    selected_list += [y_name]
     return df[selected_list]
 
 def get_highly_correlated_columns_to_remove(correlated_dict: pd.DataFrame) -> list:
@@ -95,7 +109,7 @@ def smooth_seven_minute_intervals(df: pd.DataFrame, columns_to_smooth: List[str]
     for col in columns_to_smooth:
 
         data = df[col].to_numpy()
-
+        print(data.shape)
         kernel_size = minutes
         kernel = np.ones(kernel_size) / kernel_size
         data_convolved = np.convolve(data, kernel, mode='same')
@@ -103,6 +117,25 @@ def smooth_seven_minute_intervals(df: pd.DataFrame, columns_to_smooth: List[str]
         smoothed_df[col] = data_convolved
 
     return smoothed_df
+
+
+def simple_smooth(df: pd.DataFrame, columns_to_smooth: List[str], window_size: int = 5) -> pd.DataFrame:
+
+    smoothed = df.copy()
+    for col in columns_to_smooth:
+        # f = SimpleExpSmoothing(df[col].to_numpy()).fit()
+        # print(f)
+        # df[col] = f.predict(df[col].to_numpy())
+        smoothed[col] = df[col].rolling(window=window_size, min_periods=1, closed='both').mean()
+
+    return smoothed
+
+def holt(df: pd.DataFrame, cols: List[str], alpha: int = 0.5):
+    for col in cols:
+        tmp = SimpleExpSmoothing(df[col]).fit(smoothing_level=alpha, optimized=False, use_brute=True).fittedvalues
+        df[col] = tmp.to_numpy().reshape((-1, 1))
+
+    return df
 
 
 if __name__ == '__main__':
@@ -119,23 +152,45 @@ if __name__ == '__main__':
     df = remove_redundant_columns(df)
     df = filter_device_groups(df, file='feature_lists/15groups.json', groups=groups, y_name=y_name)
 
-    print(df[y_name].iloc[:50])
+    print(df[y_name].iloc[:10])
 
-    ret = smooth_seven_minute_intervals(df, [y_name], minutes=20)
-
-    print(ret[y_name].iloc[:50])
+    #ret = smooth_seven_minute_intervals(df, [y_name], minutes=20)
+    ret = holt(df, [y_name])
+    print(ret[y_name].iloc[:10])
 
 
     import matplotlib.pyplot as plt
 
 
-    to_plot1 = df[y_name].iloc[:1000]
-    to_plot2 = ret[y_name].iloc[:1000]
+    to_plot1 = df[y_name].iloc[200:500]
+    to_plot2 = ret[y_name].iloc[200:500]
 
 
     fig = plt.figure(figsize=(15,15))
-    plt.plot(range(len(to_plot1)), to_plot1, label='original')
+    #plt.plot(range(len(to_plot1)), to_plot1, label='original')
     plt.plot(range(len(to_plot2)), to_plot2, label='smoothed')
     plt.legend()
     plt.tight_layout()
     plt.show()
+
+
+    # # TEST device
+    # hc = 'HC212B'
+    # #prefix = f'tr_20230127213409_{hc}_'
+    # y_name = f'LMAM_HC212B_PL-------_TPS'
+    # groups = ["1", "4", "5"]
+
+
+    # # hc = 'HC201A'
+    # # y_name = f'LMAM_{hc}_PLKL90---_TPG'
+    # # groups = ["1", "2", "3"]
+
+    # #df = pd.read_csv(f'dumbdata/{prefix}{y_name}.csv')
+    # df = pd.read_csv(f'maybe.csv')
+
+    # df = remove_blacklisted(df, file='feature_lists/blacklist15.txt')
+    # print('LMAM_HC212B_NDXRPI1--_PV' in df.columns)
+    # df = remove_redundant_columns(df)
+    # print('LMAM_HC212B_NDXRPI1--_PV' in df.columns)
+    # df = filter_device_groups(df, file='feature_lists/15groups.json', groups=groups, y_name=y_name, anti_groups=['y2'])
+    # print(df.columns.value_counts())
